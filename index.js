e5d5560e1b7a9865e14b7e4ee6181ca35ef118bd
SoumyaRanjanBhana1 <soumyabhanja180@gmail.com>
2025-06-21 16:23:22 +0530

    commited for server

--- index.js
+++ index.js
@@ -11,10 +11,16 @@ app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log('MongoDB Connected'))
-.catch(err => console.log(err));
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
+// Routes
 app.use('/api/auth', authRoutes);
+
+// Root route to prevent "Cannot GET /"
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
